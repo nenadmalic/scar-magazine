279e982c13985223c1d003c2166b9667ebbd9418
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-05 18:24:24 +0200

    Section containing metal reviews.

---
+++ src/routes/SectionReviews.svelte
@@ -0,0 +1,236 @@
+<section class="reviews">
+   
+    <div class="img-large">
+    <img src="https://f4.bcbits.com/img/a3182621103_65" alt="Angel Corpse" />
+
+    <div class="band-name-and-country">
+        <p>AND YOU WILL KNOW US BY THE TRAIL OF DEAD</p>
+        <div class="country-flag">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 16" width="22" height="16">
+                <rect width="25" height="15" fill="#FFF"/>
+                <g fill="#CE1124">
+                    <rect width="3" height="15" x="11"/>
+                    <rect width="25" height="3" y="6"/>
+                </g>
+            </svg>
+        </div>
+
+    </div>
+
+    <h2>Paul Di'Anno On Meeting Bruce Dickinson In Croatia: 'Everyone Thinks That We Hated Each Other, Which Is Bulls***'</h2>
+
+    <br />
+    Vreid
+    <br />
+    Nakon dugog izbivanja i rehabilitacije, početkom ovog stoljeća Carlson se vraća s novom postavom i 2005. izdaje najnoviji Earthov uradak "Hex (Or Printing in Infernal Method)".
+   </div>
+
+   <div class="img-mid">
+
+        <div class="review">
+
+                <img src="https://metal-exposure.com/wp-content/uploads/Vreid-Lifehunger-1.jpg" alt="Vreid" />
+                
+                <div class="band-name-and-country">
+                    <p>Vreid</p>
+                    <div class="country-flag">
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 22 16">
+                            <title>Flag of Norway</title>
+                            <rect width="22" height="16" fill="#ba0c2f"/>
+                            <path d="M0,8h22M8,0v16" stroke="#fff" stroke-width="4"/>
+                            <path d="M0,8h22M8,0v16" stroke="#00205b" stroke-width="2"/>
+                        </svg>
+                    </div>
+
+                </div>
+
+                <h2>Slipknot's clown is "proud" of Gojira's performance at Olympic Games Opening Ceremony: "I got goosebumps"</h2>
+
+            </div>
+        
+            <div class="review">
+
+                <!-- Immortal: https://f4.bcbits.com/img/a2174810538_65 -->
+                <img src="https://www.normanrecords.com/artwork/large/40/198540-gunship-unicorn.jpg" alt="Immortal" />
+
+                <div class="band-name-and-country">
+                    <p>Immortal</p>
+                    <div class="country-flag">
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 22 16">
+                            <title>Flag of Norway</title>
+                            <rect width="22" height="16" fill="#ba0c2f"/>
+                            <path d="M0,8h22M8,0v16" stroke="#fff" stroke-width="4"/>
+                            <path d="M0,8h22M8,0v16" stroke="#00205b" stroke-width="2"/>
+                        </svg>
+                    </div>
+
+                </div>
+
+                <h2>Mastodon Could Release New Song 'Sooner Than You Think'</h2>
+
+            </div>
+        
+            <div class="review">
+
+                <img src="https://www.angrymetalguy.com/wp-content/uploads/2022/09/Nordjevel-Gnavhol-01-768x768.jpg" alt="Angel Corpse" />
+
+                <div class="band-name-and-country">
+                    <p>THE WORLD IS A BEAUTIFUL PLACE & I AM NO LONGER AFRAID TO DIE</p>
+                    <div class="country-flag">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="18" viewBox="0 0 7410 3900">
+                            <rect width="7410" height="3900" fill="#b22234"/>
+                            <path d="M0,450H7410m0,600H0m0,600H7410m0,600H0m0,600H7410m0,600H0" stroke="#fff" stroke-width="300"/>
+                            <rect width="2964" height="2100" fill="#3c3b6e"/>
+                            <g fill="#fff">
+                            <g id="s18">
+                            <g id="s9">
+                            <g id="s5">
+                            <g id="s4">
+                            <path id="s" d="M247,90 317.534230,307.082039 132.873218,172.917961H361.126782L176.465770,307.082039z"/>
+                            <use xlink:href="#s" y="420"/>
+                            <use xlink:href="#s" y="840"/>
+                            <use xlink:href="#s" y="1260"/>
+                            </g>
+                            <use xlink:href="#s" y="1680"/>
+                            </g>
+                            <use xlink:href="#s4" x="247" y="210"/>
+                            </g>
+                            <use xlink:href="#s9" x="494"/>
+                            </g>
+                            <use xlink:href="#s18" x="988"/>
+                            <use xlink:href="#s9" x="1976"/>
+                            <use xlink:href="#s5" x="2470"/>
+                            </g>
+                        </svg>
+                    </div>
+
+                </div>
+
+                <h2>John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+
+            </div>
+
+            <div class="review">
+
+                <img src="https://i.scdn.co/image/ab67616d0000b2731a115512e47ae33971bf3a5a" alt="Vreid" />
+                
+                <div class="band-name-and-country">
+                    <p>AND YOU WILL KNOW US BY THE TRAIL OF DEAD</p>
+                    <div class="country-flag">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 16" width="22" height="16">
+                            <rect width="25" height="15" fill="#FFF"/>
+                            <g fill="#CE1124">
+                                <rect width="3" height="15" x="11"/>
+                                <rect width="25" height="3" y="6"/>
+                            </g>
+                        </svg>
+                    </div>
+
+                </div>
+
+                <h2>Paul Di'Anno On Meeting Bruce Dickinson In Croatia: 'Everyone Thinks That We Hated Each Other, Which Is Bulls***'</h2>
+
+            </div>
+        
+            <div class="review">
+
+                <img src="https://f4.bcbits.com/img/a2571377543_65" alt="Immortal" />
+
+                <div class="band-name-and-country">
+                    <p>Carpeter Brut</p>
+                    <div class="country-flag">
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 22 16">
+                            <title>Flag of Norway</title>
+                            <rect width="22" height="16" fill="#ba0c2f"/>
+                            <path d="M0,8h22M8,0v16" stroke="#fff" stroke-width="4"/>
+                            <path d="M0,8h22M8,0v16" stroke="#00205b" stroke-width="2"/>
+                        </svg>
+                    </div>
+
+                </div>
+
+                <h2>Mastodon Could Release New Song 'Sooner Than You Think'</h2>
+
+            </div>
+        
+            <div class="review">
+
+                <img src="https://f4.bcbits.com/img/a3732326573_65" alt="Angel Corpse" />
+
+                <div class="band-name-and-country">
+                    <p>THE WORLD IS A BEAUTIFUL PLACE & I AM NO LONGER AFRAID TO DIE</p>
+                    <div class="country-flag">
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 22 16">
+                            <title>Flag of Norway</title>
+                            <rect width="22" height="16" fill="#ba0c2f"/>
+                            <path d="M0,8h22M8,0v16" stroke="#fff" stroke-width="4"/>
+                            <path d="M0,8h22M8,0v16" stroke="#00205b" stroke-width="2"/>
+                        </svg>
+                    </div>
+
+                </div>
+
+                <h2>John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+
+            </div>
+        </div> <!-- /img-mid -->
+</section>
+
+<style>
+        section.reviews div.band-name-and-country {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    section.reviews div.band-name-and-country p {
+        text-transform: uppercase;
+        margin: 20px 0;
+        width: 87%;
+    }
+    section.reviews div.review h2 {
+        margin: 0 0 20px;
+    }
+    section.reviews div.country-flag {
+        width: 22px;
+        height: 16px;
+        margin: 20px 0 0;
+        display: flex;
+        align-items: flex-start;
+        align-self: self-start;
+    }
+    section.reviews {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+            width: 100%;
+            height: auto;
+            background-color: black;
+    }
+    section div.img-large {
+        width: 40%;
+        margin: 1.5%;
+    }
+    section div.img-large img {
+        width: 100%;
+    }
+    div.img-mid {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: flex-start;
+        width: 55.5%;
+        margin: 1.5% 1.5% 0 0;
+    }
+    section div.review {
+        width: 31.3%;
+    }
+    section div.review:nth-last-of-type(2) {
+        margin: 0 3%;
+    }
+    section div.review:nth-child(n+4) {
+        margin-top: 0; /* Remove top margin for second row */
+    }
+    section div.review img {
+        width: 100%;
+    }
+</style>
