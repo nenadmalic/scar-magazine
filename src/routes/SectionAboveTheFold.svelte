e78f923a4e4d8e7cccfb1719758692350baadc79
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-05 18:45:02 +0200

    Above the fold section.

---
+++ src/routes/SectionAboveTheFold.svelte
@@ -0,0 +1,158 @@
+<div class="top-row">
+    <section class="topSlider">
+
+        <div class="content">
+            
+            <div class="text">
+                <p>Recenzije: Zombie Holocaust * Filmovi <span>Read more →</span></p>
+                <h2 class="inter-light">VOLBEAT Frontman MICHAEL POULSEN's Death Metal Project ASINHELL Plays Final Concert Of 'Impii Hora' Touring Cycle</h2>
+                <p>Fan-filmed video of the performance can be seen below.</p>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="topNews">
+
+        <div class="content">
+
+            <div class="news">
+                <img src="https://i.scdn.co/image/ab67616d0000b2731a115512e47ae33971bf3a5a" alt="Vreid" />
+                <h2 class="inter-light">Volbet Frontman Michael Poulsen's Death Metal Project Asinhell Plays Final Concert Of 'Impii Hora' Touring Cycle</h2>
+            </div>
+
+            <div class="news">
+                <img src="https://www.normanrecords.com/artwork/large/40/198540-gunship-unicorn.jpg" alt="Immortal" />
+                <h2>Paul Di'Anno On Meeting Bruce Dickinson In Croatia: 'Everyone Thinks That We Hated Each Other, Which Is Bulls***'</h2>
+            </div>
+
+            <div class="news">
+                <img src="https://f4.bcbits.com/img/a3182621103_65" alt="Angel Corpse" />
+                <h2>John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+            </div>
+
+            <div class="news">
+                <img src="https://www.normanrecords.com/artwork/large/40/198540-gunship-unicorn.jpg" alt="Immortal" />
+                <h2>Paul Di'Anno On Meeting Bruce Dickinson In Croatia: 'Everyone Thinks That We Hated Each Other, Which Is Bulls***'</h2>
+            </div>
+
+            <div class="news">
+                <img src="https://www.metal-archives.com/images/1/0/8/8/1088712.jpg" alt="Klone" />
+                <h2>John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+            </div>
+
+        </div>
+
+    </section>
+</div>
+
+<!--
+<p>REVIEWS</p>
+-->
+
+
+
+<style>
+
+
+    .top-row {
+        display: flex;
+        width: 100%;
+    }
+
+    section.topSlider {
+        width: 65%;
+        padding: 1.5% 0 0;
+        background-color: black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    section.topSlider div.content {
+        width: 100%;
+        height: 60vh;
+        display: flex;
+        align-items: center;
+        background-image: url('https://m.media-amazon.com/images/M/MV5BNmNkYjczOTQtNTQxZC00MzIyLTk5ZTktNjE5NTlmYjJlNmY2XkEyXkFqcGdeQXVyMTU0NTE4MTkz._V1_FMjpg_UX1000_.jpg');
+        background-size: 70%;
+        background-position: top 12% right -3%;
+        background-repeat: no-repeat;
+    }
+
+    section.topSlider div.content div.text {
+        width: 32.5%;
+        margin: 8% auto auto 2.4%;
+    }
+
+    section.topSlider div.content div.text p:nth-last-of-type(1) {
+        font-size: 1.25rem;
+        color: orange;
+    }
+
+    section.topSlider div.content div.text h2 {
+        font-size: 1.75rem;
+        color: #f5f5f5;
+    }
+
+    section.topSlider div.content div.text p:nth-last-of-type(2) {
+        font-size: 1.25rem;
+    }
+
+    section.topNews {
+        width: 35%;
+        padding: 1.5%;
+        background-color: black;
+        color: #f5f5f5;
+        display: flex;
+        align-items: flex-start;
+    }
+        .news {
+            display: flex;
+            align-items: flex-start;
+            margin: 1.6rem 0 0 0;
+        }
+        .news:first-child {
+            margin-top: 0;
+        }
+        .news:last-child {
+            margin-bottom: 0;
+        }
+
+        .news img {
+            width: 17%; /* Adjust as needed */
+            margin-right: 1.6rem;
+        }
+
+        .news h2 {
+            margin: 0;
+            flex: 1;
+        }
+
+       /* <!-- 
+       
+        2nd section ///  topReview
+    
+        --> */
+
+
+
+    @media (max-width: 768px) {
+        section.topSlider {
+            min-height: 20vh;
+        }
+        section.topSlider div.content {
+            flex-direction: column;
+        }
+        section.topSlider div.content div.text {
+            width: 100%;
+            text-align: center;
+            padding-right: 0;
+        }
+        section.topSlider div.content div.image {
+            width: 100%;
+            margin-top: 1rem;
+        }
+    }
+</style>
