84cd5772ebd62bb926e046b76e001eb2b3b84f01
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-09 01:01:02 +0200

    Front page section for movie reviews.

---
+++ src/routes/SectionMovies.svelte
@@ -0,0 +1,91 @@
+<div class="d-flex align-items-center bg-white text-black py-4 pb-4">
+    <img src="/skull-zombie.png" alt="Scar Skull" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0 0;">
+    <h1 class="me-3 mb-0"><b>Movies</b></h1>
+    <div class="mb-0 text-black">Latest movie reviews: Moonspell – Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.</div>
+</div>
+
+<div class="container-fluid px-4 pb-5 pt-5">
+    <div class="row mb-5" style="
+            height: 700px;
+            background-image: url('https://m.media-amazon.com/images/M/MV5BNmNkYjczOTQtNTQxZC00MzIyLTk5ZTktNjE5NTlmYjJlNmY2XkEyXkFqcGdeQXVyMTU0NTE4MTkz._V1_FMjpg_UX1000_.jpg');
+            background-repeat: no-repeat;
+            background-position: top 10% right 5%;
+            background-size: 70%;
+            ">
+        <div class="col-12">
+            <h2 class="text-white">ZOMBIE HOLOCAUST</h2>
+            <!-- Add content for the featured movie here -->
+        </div>
+    </div>
+    
+    <div class="row gx-5">
+        <div class="col-md-2 mb-5 px-4">
+            <img src="https://m.media-amazon.com/images/M/MV5BYTFkM2ViMmQtZmI5NS00MjQ2LWEyN2EtMTI1ZmNlZDU3MTZjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg" alt="28 days later movie cover" class="img-fluid">
+            
+            <p class="text-uppercase">28 DAYS LATER</p>
+            
+            <hr />
+            
+            <h2 class="inter-light">John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+        </div>
+
+        <div class="col-md-2 mb-5 px-4">
+            <img src="https://images.justwatch.com/poster/259100069/s718/zombie-lake.jpg" alt="Zombie Lake cover" class="img-fluid">
+            
+            <p class="text-uppercase">ZOMBIE LAKE</p>
+  
+            <hr />
+            
+            <h2 class="inter-light">Slipknot's clown is "proud" of Gojira's performance at Olympic Games Opening Ceremony: "I got goosebumps"</h2>
+        
+        </div>
+
+        <div class="col-md-2 mb-5 px-4">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/71/Dead_Alive_%281993%29.png" alt="Braindead movie cover" class="img-fluid">
+            
+            <p class="text-uppercase">Braindead</p>
+                
+            <hr />
+            
+            <h2 class="inter-light">Synthwave pioneers discuss their latest album and the intersection of metal and electronic music</h2>
+        
+        </div>
+        
+        <div class="col-md-2 mb-5 px-4">
+            <img src="https://m.media-amazon.com/images/M/MV5BZmUzZWYzOTgtZDQ4OS00ZGYzLTlmMWYtN2E5ZDJmNjM4MDFmXkEyXkFqcGdeQXVyMzU4ODM5Nw@@._V1_FMjpg_UX1000_.jpg" alt="Zeal & Ardor" class="img-fluid">
+            
+            <p class="text-uppercase">CHILDREN OF THE NIGHT</p>
+                
+            <hr />
+            
+            <h2 class="inter-light">Manuel Gagneux talks about blending black metal with African-American spirituals</h2>
+        
+        </div>
+        
+        <div class="col-md-2 mb-5 px-4">
+            <img src="https://m.media-amazon.com/images/I/81IsA-FuQML._AC_UF894,1000_QL80_.jpg" alt="Demons 2 movie cover" class="img-fluid">
+            
+            <p class="text-uppercase">DEMONS 2</p>
+                
+            <hr />
+            
+            <h2 class="inter-light">Exploring the evolution of blackgaze and post-metal with Deafheaven's George Clarke</h2>
+        </div>
+        
+        <div class="col-md-2 mb-5 px-4">
+            <img src="https://images.static-bluray.com/products/20/29449_1_front.jpg" alt="Devil Rider" class="img-fluid">
+            
+            <p class="text-uppercase">Devil Rider</p>
+                
+            <hr />
+            
+            <h2 class="inter-light">James Kent discusses the dark future of electronic music and his metal roots</h2>
+        </div>
+    </div>
+</div>
+
+<style>
+    .container-fluid {
+        background-color: #0a001e;
+    }
+</style>
