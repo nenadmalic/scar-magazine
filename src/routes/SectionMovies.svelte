54595728a5716c7dbdef1fc793084f6f60704faa
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-09 21:41:54 +0200

    Testing out various design styles.

--- src/routes/SectionMovies.svelte
+++ src/routes/SectionMovies.svelte
@@ -1,24 +1,54 @@
-<div class="d-flex align-items-center bg-white text-black py-4 pb-4">
-    <img src="/skull-zombie.png" alt="Scar Skull" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0 0;">
+<div class="d-flex align-items-center bg-white text-black py-4 pb-4" style="
+        background-image: url('/skull/skull-zombie-8.webp');
+        background-repeat: no-repeat;
+        background-position: bottom 140% right;
+        background-size: 140px;
+
+    ">
+    <img src="/skull/skull-zombie-9.webp" alt="Scar Skull" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0 0;border:1px solid black">
     <h1 class="me-3 mb-0"><b>Movies</b></h1>
     <div class="mb-0 text-black">Latest movie reviews: Moonspell – Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.</div>
 </div>
 
-<div class="container-fluid px-4 pb-5 pt-5">
-    <div class="row mb-5" style="
-            height: 700px;
-            background-image: url('https://m.media-amazon.com/images/M/MV5BNmNkYjczOTQtNTQxZC00MzIyLTk5ZTktNjE5NTlmYjJlNmY2XkEyXkFqcGdeQXVyMTU0NTE4MTkz._V1_FMjpg_UX1000_.jpg');
-            background-repeat: no-repeat;
-            background-position: top 10% right 5%;
-            background-size: 70%;
-            ">
-        <div class="col-12">
-            <h2 class="text-white">ZOMBIE HOLOCAUST</h2>
-            <!-- Add content for the featured movie here -->
+
+<div class="col-12 px-4 pb-5 pt-5 mx-auto" style="
+    background-image: url('/zombie/zombie-girl-5.png');
+    background-repeat: no-repeat;
+    background-position: bottom left;
+    background-size: 25%;
+    padding-left: 25%;
+    background-color:black;
+    ">
+
+
+    <div class="row justify-content-center gx-5">
+        <div class="col-lg-8 px-4">
+
+            <div class="row mb-5" style="
+                    height: 600px;
+                    background-image: url('https://m.media-amazon.com/images/M/MV5BNmNkYjczOTQtNTQxZC00MzIyLTk5ZTktNjE5NTlmYjJlNmY2XkEyXkFqcGdeQXVyMTU0NTE4MTkz._V1_FMjpg_UX1000_.jpg');
+                    background-repeat: no-repeat;
+                    background-position: top 20% right -30%;
+                    background-size: 70%;
+                    ">
+                <div class="text">
+                    <h2>ZOMBIE HOLOCAUST</h2>
+
+                    <hr />
+                    
+                    <blockquote>
+                        <p>"When science meddles with nature, the only result is disaster... and zombies!"</p>
+                    </blockquote>
+
+                    <hr />
+
+                    <p>Scientists uncover a terrifying secret on a remote Caribbean island, facing a threat that blends science with savagery. "Zombie Holocaust" shocks with its graphic violence and exotic setting, pushing horror to its limits. Discover why this controversial cult classic still captivates decades later.</p>
+                </div>
+            </div>
         </div>
     </div>
     
-    <div class="row gx-5">
+    <div class="row gx-5" style="padding-left: 25%;">
         <div class="col-md-2 mb-5 px-4">
             <img src="https://m.media-amazon.com/images/M/MV5BYTFkM2ViMmQtZmI5NS00MjQ2LWEyN2EtMTI1ZmNlZDU3MTZjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg" alt="28 days later movie cover" class="img-fluid">
             
@@ -84,8 +114,43 @@
     </div>
 </div>
 
+
+<div class="d-flex align-items-center bg-white text-black py-4 pb-4" style="
+        height:400px;
+        background-image: url('/blood/blood-6.webp');
+        background-repeat: repeat-x;
+        background-position: top center;
+        background-size: 45%;
+
+    ">
+</div>
+
+<div class="d-flex flex-column justify-content-end bg-white text-black py-4 pb-4" style="height: 400px;">
+    <div class="d-flex align-items-center">
+        <img src="/skull/skull-zombie-9.webp" alt="Scar Skull" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0 0;border:1px solid black">
+        <h1 class="me-3 mb-0"><b>ScarMag</b></h1>
+    </div>
+    <div class="mt-3 text-black">Latest movie reviews: Moonspell – Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.</div>
+</div>
+
+
+
 <style>
     .container-fluid {
-        background-color: #0a001e;
+        background-color: #06050D;
+    }
+    blockquote p {
+        color: aliceblue;
+        font-size: 2rem;
+        text-align: center;
+        padding: 3rem 0 3rem;
+    }
+    .text {
+        max-width: 45%;
+        margin:2rem 0 4rem;
+        background-color: rgba(0, 0, 0, 0.7);
+    }
+    .text h2 {
+        color: gold;
     }
 </style>
