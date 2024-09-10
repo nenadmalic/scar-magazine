c5b8e34563e1cf5d69fe7c77b184c60c1e544676
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-10 17:04:02 +0200

    Location and file name updated.

---
+++ src/routes/SectionNews.svelte
@@ -0,0 +1,195 @@
+<div class="container-fluid px-4 pb-5">
+    <!-- First row -->
+    <div class="row gx-5 mb-5">
+        <div class="col-lg-5 col-md-6 col-sm-12">
+            <img src="https://label.napalmrecords.com/templates/yootheme/cache/2d/2024-samael-header-2d1c7899.webp" alt="Samael">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p>SAMAEL</p>
+
+                <div class="country-flag ms-2 mt-0">
+                    <img src="/flags/switzerland.svg" alt="Flag of Switzerland" />
+                </div>
+            </div>
+            
+            <hr />
+            <h2 class="inter-light">Volbet Frontman Michael Poulsen's Death Metal Project Asinhell Plays Final Concert Of 'Impii Hora' Touring Cycle
+                <datetime>3 minutes ago</datetime>
+            </h2>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-12">
+            <img src="https://label.napalmrecords.com/templates/yootheme/cache/fd/2022-satyricon-band-pic-web-fdc2da4d.webp" alt="Satyricon">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p>SATYRICON</p>
+
+                <div class="country-flag ms-2 mt-0">
+                    <img src="/flags/norway.svg" alt="Flag of Norway" />
+                </div>
+            </div>
+            
+            <hr />
+            <h2 class="inter-light">Slipknot's's clown is 'proud' of Gojira's performance at Olympic Games Opening Ceremony: 'I got goosebumps'
+                <datetime>2 days ago</datetime>
+            </h2>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-12">
+            <div class="d-flex justify-content-between align-items-start">
+                <p>KATATONIA</p>
+
+                <div class="country-flag ms-2 mt-0">
+                    <img src="/flags/sweden.svg" alt="Flag of Sweden" />
+                </div>
+            </div>
+
+            <hr />
+            <h2 class="inter-light">Volbet Frontman Michael Poulsen's Death Metal Project Asinhell Plays Final Concert Of 'Impii Hora' Touring Cycle
+                <datetime>September 2, 2024</datetime>
+            </h2>
+            <img src="https://label.napalmrecords.com/templates/yootheme/cache/22/2023-katatonia-band-picture-web-229721ed.webp" alt="Katatonia">
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <img src="https://label.napalmrecords.com/images/bands/batushka/2022-batushka-band-picture-press.jpg" alt="Samael">
+
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p>BATUSHKA</p>
+
+                <div class="country-flag ms-2 mt-0">
+                    <img src="/flags/poland.svg" alt="Flag of Poland" />
+                </div>
+            </div>
+            <hr />
+            <h2 class="inter-light">Although the band is Polish, the language they are singing in is Church Slavonic, the language used in the Orthodox Church in parts of ...
+                <datetime>65 minutes ago</datetime>
+            </h2>
+        </div>
+    </div>
+
+    <!-- Second row -->
+    <div class="row mb-5">
+        <!-- First column (2/5 width on large screens, 50% on medium) -->
+        <div class="col-lg-5 col-md-6 col-sm-12">
+            <div class="row mb-3">
+                <div class="col-4">
+                    <img src="https://media.nuclearblast.de/apps/media/lounge/foto/MachineHead2022a.jpg" alt="Machine Head" class="small-image">
+                </div>
+                <div class="col-md-8 col-sm-12 col-lg-7">
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>MACHINE HEAD</p>
+
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/united-states.svg" alt="Flag of United States" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="inter-light">Fear Factory will join Bay Area icons Machine Head in Australia and New Zealand 2024! Tickets – click here or go to the Tour Dates page.
+                        <datetime>4 days ago</datetime>
+                    </h2>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-4">
+                    <img src="https://consequence.net/wp-content/uploads/2023/12/Sepultura-Image.jpg" alt="Sepultura" class="small-image">
+                </div>
+                <div class="col-md-8 col-sm-12 col-lg-7">
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>SEPULTURA</p>
+
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/brazil.svg" alt="Flag of Brazil" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="inter-light">Sepultura announces final world tour 'Celebrating Life Through Death' for 2024. Don't miss their last shows ever!
+                        <datetime>1 day ago</datetime>
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Second column (3/5 width on large screens, 50% on medium) -->
+        <div class="col-lg-7 col-md-6 col-sm-12">
+            <div class="row">
+                
+                <div class="col-lg-8 col-md-12">
+                    <img src="/fear-factory-2023.jpg" alt="Fear Factory" class="large-image">
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex flex-column justify-content-end">
+                        <div class="mt-3 d-flex justify-content-between align-items-start">
+                            <p>FEAR FACTORY</p>
+
+                            <div class="country-flag ms-2 mt-0">
+                                <img src="/flags/united-states.svg" alt="Flag of United States" />
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <h2 class="inter-light">Fear Factory will join Bay Area icons Machine Head in Australia and New Zealand 2024! Tickets – click here or go to the Tour Dates page.
+                            <datetime>3 minutes ago</datetime>
+                        </h2>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Third row -->
+    <div class="row">
+        <!-- First column (2/5 width on large screens, 50% on medium) -->
+        <div class="col-lg-5 col-md-6 col-sm-12">
+            <div class="row">
+                <div class="col-4">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/Absu_31_10_2009_Paris_06.jpg" alt="Machine Head" class="small-image">
+                </div>
+                <div class="col-md-8 col-sm-12 col-lg-7">
+
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>PROSCRIPTORS MCGOVERN'S APSU</p>
+
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/united-states.svg" alt="Flag of United States" />
+                        </div>
+                    </div>
+
+                    <hr />
+
+                    <h2 class="inter-light">Fear Factory will join Bay Area icons Machine Head in Australia and New Zealand 2024! Tickets – click here or go to the Tour Dates page.
+                        <datetime>13 minutes ago</datetime>
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Second column (3/5 width on large screens, 50% on medium) -->
+        <div class="col-lg-7 col-md-6 col-sm-12">
+            <div class="row">
+                <div class="col-lg-8 col-md-6">
+                    <img src="https://www.nuclearblast.com/cdn/shop/articles/soulfly.jpg?v=1717686487&width=1500" alt="Soulfly" class="large-image">
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex flex-column justify-content-end">
+                        <div class="mt-3 d-flex justify-content-between align-items-start">
+                            <p>SOULFLY</p>
+
+                            <div class="country-flag ms-2 mt-0">
+                                <img src="/flags/brazil.svg" alt="Flag of Brazil" />
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <h2 class="inter-light">Slipknot's's clown is 'proud' of Gojira's performance at Olympic Games Opening Ceremony: 'I got goosebumps'.
+                            <datetime>2 days ago</datetime>
+                        </h2>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<style>
+
+</style>
