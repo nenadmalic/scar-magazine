0d5cb0e5c6609754fe5f58507b244aa16c1a3e49
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-10 17:05:53 +0200

    Background color added.

---
+++ src/routes/AboveTheFold.svelte
@@ -0,0 +1,132 @@
+<div class="container-fluid px-4 py-5">
+    <!-- First row -->
+    <div class="row gx-5">
+        
+        <div class="col-lg-2 col-md-6 col-sm-12">
+            <div class="row mb-3">
+                <div class="col-12">
+                    <img src="https://label.napalmrecords.com/templates/yootheme/cache/fd/2022-satyricon-band-pic-web-fdc2da4d.webp" alt="Satyricon">
+                    <div class="mt-3 d-flex justify-content-between align-items-start">
+                        <p>SATYRICON</p>
+        
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/norway.svg" alt="Flag of Norway" />
+                        </div>
+                    </div>
+                    
+                    <hr />
+                    <h2 class="inter-light">Slipknot's clown is 'proud' of Gojira's performance at Olympic Games Opening Ceremony: 'I got goosebumps'
+                        <datetime>2 days ago</datetime>
+                    </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <img src="https://i0.wp.com/heavymusichq.com/wp-content/uploads/2021/07/Mayhem.jpg?ssl=1" alt="Mayhem">
+                    <div class="mt-3 d-flex justify-content-between align-items-start">
+                        <p>MAYHEM</p>
+        
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/norway.svg" alt="Flag of Norway" />
+                        </div>
+                    </div>
+                    
+                    <hr />
+                    <h2 class="inter-light">Mayhem announces 40th anniversary tour, promising a journey through their controversial history
+                        <datetime>1 day ago</datetime>
+                    </h2>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-lg-7 col-md-8 col-sm-12">
+            <img src="/watain-2022.jpg" alt="Watain">
+            <p class="mt-3 main-headline-band">WATAIN</p>
+            <hr class="main-headline-divider bg-white" />
+            <h1 class="main-headline-title text-white">Volbet Frontman Michael Poulsen's Death Metal Project Asinhell Plays Final Concert Of 'Impii Hora' Touring Cycle</h1>
+            <p class="main-headline-time text-white"><datetime>3 minutes ago</datetime></p>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="d-flex mb-4">
+                <div style="flex: 0 0 60%;">
+                    
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>KATATONIA</p>
+
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/sweden.svg" alt="Flag of Sweden" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="inter-light">Volbet Frontman Michael Poulsen's Death Metal Project Asinhell Plays Final Concert Of 'Impii Hora' Touring Cycle
+                        <datetime>September 2, 2024</datetime>
+                    </h2>
+                </div>
+                <div class="ms-2" style="flex: 0 0 40%;">
+                    <img src="https://label.napalmrecords.com/templates/yootheme/cache/22/2023-katatonia-band-picture-web-229721ed.webp" alt="Katatonia" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="d-flex align-items-start mb-4">
+                <img src="https://i.scdn.co/image/ab67616d0000b2731a115512e47ae33971bf3a5a" alt="Vreid" class="w-25 me-3" />
+                <div>
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>VREID</p>
+        
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/norway.svg" alt="Flag of Norway" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="h6 fw-light mb-0">Volbet Frontman Michael Poulsen's Death Metal Project Asinhell Plays Final Concert Of 'Impii Hora' Touring Cycle</h2>
+                    <datetime>3 minutes ago</datetime>
+                </div>
+            </div>
+
+            <div class="d-flex align-items-start mb-4">
+                <img src="https://www.normanrecords.com/artwork/large/40/198540-gunship-unicorn.jpg" alt="Immortal" class="w-25 me-3" />
+                <div>
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>IMMORTAL</p>
+        
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/norway.svg" alt="Flag of Norway" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="h6 mb-0">Paul Di'Anno On Meeting Bruce Dickinson In Croatia: 'Everyone Thinks That We Hated Each Other, Which Is Bulls***'</h2>
+                    <datetime>3 hours ago</datetime>
+                </div>
+            </div>
+
+            <div class="d-flex align-items-start mb-4">
+                <img src="https://f4.bcbits.com/img/a3182621103_65" alt="Angel Corpse" class="w-25 me-3" />
+                <div>
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>ANGELCORPSE</p>
+        
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/united-states.svg" alt="Flag of United States" />
+                        </div>
+                    </div>
+                    
+                    <p class="mb-0"></p>
+                    <hr />
+                    <h2 class="h6 mb-0">John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+                    <datetime>23 hours ago</datetime>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<style>
+    .container-fluid {
+        background-color: black;
+    }
+</style>
