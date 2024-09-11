02d1bac0636fd4375ed7af6027dbad7a9c3eb67b
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-11 23:14:30 +0200

    File name updated.

---
+++ src/routes/ScarskinLatestNews.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let data;
+    const latestNews = data.props.latestNews;
+</script>
+  
+    <table class="content-list-frontpage">
+        <thead>
+            <tr>
+                <th>Objavljene novosti</th>
+                <th>Datum</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each latestNews as n}
+            <tr>
+                <td>
+                    <a href="{n.content_id}">
+                        {n.content_title}
+                    </a>
+                </td>
+                <td>
+                    {n.content_published_at}
+                </td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
+  
+  <style>
+        .content-list-frontpage {
+            width: 100%;
+            }
+        a {
+            color: #000;
+            text-decoration: none;
+            }
+        a:hover {
+            color: #f00;
+            }
+  </style>
