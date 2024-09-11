2c9869c7ce0bebfd3de471bc4267a6db7e55f907
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-11 23:07:45 +0200

    New layout with three row, plus a new section title design.

---
+++ src/routes/SectionEvents.svelte
@@ -0,0 +1,219 @@
+
+
+<div class="d-flex align-items-end text-black py-3 pb-1" style="
+        background-image: url('/skull/skull-screaming-4.webp');
+        background-repeat: no-repeat;
+        background-position: bottom 140% left -1%;
+        background-size: 140px;
+        padding-left:100px;
+        min-height: 100px;
+        z-index: 1;
+        position: relative;
+        ">
+    <!--
+    <img src="/skull/skull-screaming-2.webp" alt="Scar Skull" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0 0;border:1px solid black;object-fit:cover;object-position:center;transform:scale(1.5);">
+    -->
+    <h1 class="me-3 mb-0"><b>Events</b></h1>
+    <!--
+    <div class="mb-0 text-black">Latest event reviews: Moonspell – Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.</div>
+    -->
+</div>
+
+
+
+<div class="container-fluid px-4 pb-5 pt-5 roundedBottom">
+
+    <div class="row mb-5 justify-content-center">
+        <div class="col-3">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p>SUFFOCATION's first Croatian gig</p>
+                
+                <div class="country-flag ms-2">
+                    <img src="/flags/united-states.svg" alt="Flag of United States" />
+                </div>
+            </div>
+
+            <hr />
+            <!--
+            <h2>
+                Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.
+            </h2>
+            -->
+            <blockquote class="text-white fs-4">
+                <p>
+                    "Suffocation je doslovno od početka do kraja gazio i komadao bez milosti sve pred sobom"
+                    <!-- Suffocation literally trampled and shredded everything before them from start to finish without mercy -->
+                </p>
+                <!--
+                <p>
+                    "The Portuguese metal scene is thriving. Bands like Manhater and Process of Guilt are pushing boundaries."
+                </p>
+                -->
+            </blockquote>
+            <p class="main-headline-time text-white">Fernando Ribeiro opens up about Moonspell's journey, from crafting their latest masterpiece to winning MTV awards and championing their local metal community. Dive into the depths of one of dark metal's most enduring voices.
+            </p>
+            <p class="main-headline-time text-secondary small">Event date & location: 15. 5. 2007. @ Mocvara, Zagreb, Croatia. &bull; Review by: Igor K.</p>
+        </div>
+        <div class="col-5 position-relative">
+
+            <div>
+                <img src="https://vms.hr/scarskin/media/suffocation/BGlive.jpg" alt="Fernando Ribeiro performing in Lisbon, Portugal on 31. 10. 2007."
+                    style="object-fit: contain; object-position: center top; border:1px solid #222;">
+            </div>
+
+
+            <div class="position-absolute bottom-0 end-0 bg-black bg-opacity-75 text-white p-2 small">
+                <div class="mb-0">
+                    <p>Photo by Nenad Malic <a href="https://creativecommons.org/licenses/by-sa/3.0" class="text-white text-decoration-none">CC BY-SA 3.0</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mb-5">
+        <!-- First column (2/5 width on large screens, 50% on medium) -->
+        <div class="col-lg-5 col-md-6 col-sm-12">
+            <div class="row mb-3">
+                <div class="col-4">
+                    <img src="https://media.nuclearblast.de/apps/media/lounge/foto/MachineHead2022a.jpg" alt="Machine Head" class="small-image">
+                </div>
+                <div class="col-md-8 col-sm-12 col-lg-7">
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>MACHINE HEAD</p>
+
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/united-states.svg" alt="Flag of United States" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="inter-light">Fear Factory will join Bay Area icons Machine Head in Australia and New Zealand 2024! Tickets – click here or go to the Tour Dates page.
+                        <datetime>4 days ago</datetime>
+                    </h2>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-4">
+                    <img src="https://consequence.net/wp-content/uploads/2023/12/Sepultura-Image.jpg" alt="Sepultura" class="small-image">
+                </div>
+                <div class="col-md-8 col-sm-12 col-lg-7">
+                    <div class="d-flex justify-content-between align-items-start">
+                        <p>SEPULTURA</p>
+
+                        <div class="country-flag ms-2 mt-0">
+                            <img src="/flags/brazil.svg" alt="Flag of Brazil" />
+                        </div>
+                    </div>
+
+                    <hr />
+                    <h2 class="inter-light">Sepultura announces final world tour 'Celebrating Life Through Death' for 2024. Don't miss their last shows ever!
+                        <datetime>1 day ago</datetime>
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Second column (3/5 width on large screens, 50% on medium) -->
+        <div class="col-lg-7 col-md-6 col-sm-12">
+            <div class="row">
+                
+                <div class="col-lg-8 col-md-12">
+                    <img src="/fear-factory-2023.jpg" alt="Fear Factory" class="large-image">
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex flex-column justify-content-end">
+                        <div class="mt-3 d-flex justify-content-between align-items-start">
+                            <p>FEAR FACTORY</p>
+
+                            <div class="country-flag ms-2 mt-0">
+                                <img src="/flags/united-states.svg" alt="Flag of United States" />
+                            </div>
+                        </div>
+
+                        <hr />
+
+                        <h2 class="inter-light">Fear Factory will join Bay Area icons Machine Head in Australia and New Zealand 2024! Tickets – click here or go to the Tour Dates page.
+                            <datetime>3 minutes ago</datetime>
+                        </h2>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- Third row -->
+        <div class="row">
+            
+            <!-- Second column (3/5 width on large screens, 50% on medium) -->
+            <div class="col-lg-7 col-md-6 col-sm-12">
+                <div class="row">
+                    <div class="col-lg-8 col-md-6">
+                        <img src="https://www.nuclearblast.com/cdn/shop/articles/soulfly.jpg?v=1717686487&width=1500" alt="Soulfly" class="large-image">
+                    </div>
+                    <div class="col-lg-4 col-md-6 d-flex flex-column justify-content-end">
+                            <div class="mt-3 d-flex justify-content-between align-items-start">
+                                <p>SOULFLY</p>
+    
+                                <div class="country-flag ms-2 mt-0">
+                                    <img src="/flags/brazil.svg" alt="Flag of Brazil" />
+                                </div>
+                            </div>
+    
+                            <hr />
+    
+                            <h2 class="inter-light">Slipknot's's clown is 'proud' of Gojira's performance at Olympic Games Opening Ceremony: 'I got goosebumps'.
+                                <datetime>2 days ago</datetime>
+                            </h2>
+    
+    
+                    </div>
+                </div>
+            </div>
+            
+            <!-- First column (2/5 width on large screens, 50% on medium) -->
+            <div class="col-lg-5 col-md-6 col-sm-12">
+                <div class="row">
+                    <div class="col-4">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/Absu_31_10_2009_Paris_06.jpg" alt="Machine Head" class="small-image">
+                    </div>
+                    <div class="col-md-8 col-sm-12 col-lg-7">
+    
+                        <div class="d-flex justify-content-between align-items-start">
+                            <p>PROSCRIPTORS MCGOVERN'S APSU</p>
+    
+                            <div class="country-flag ms-2 mt-0">
+                                <img src="/flags/united-states.svg" alt="Flag of United States" />
+                            </div>
+                        </div>
+    
+                        <hr />
+    
+                        <h2 class="inter-light">Fear Factory will join Bay Area icons Machine Head in Australia and New Zealand 2024! Tickets – click here or go to the Tour Dates page.
+                            <datetime>13 minutes ago</datetime>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+
+<style>
+    .container-fluid {
+        background: linear-gradient(to bottom, #190e1b, #000000);
+        /* #4A0202; */
+    }
+    blockquote p {
+        color: aliceblue;
+        font-size: 2rem;
+        text-align: center;
+        padding: 1rem 0 4.5rem;
+        font-style: italic;
+    }
+    img {
+        border: 1px solid #222;
+    }
+    .roundedBottom {   
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+    }
+</style>
