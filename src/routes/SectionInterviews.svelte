e25c64f23b26816890a64e71d46514effeb31e42
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-11 02:26:57 +0200

    Update to the layout. Images added.

---
+++ src/routes/SectionInterviews.svelte
@@ -0,0 +1,133 @@
+<div class="d-flex bg-white align-items-center text-black py-4 pb-4">
+    <img src="/skull-dark-purple.png" alt="Scar Skull" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0 0;">
+    <h1 class="me-3 mb-0"><b>Interviews</b></h1>
+    <div class="mb-0 text-black">Latest interview: Moonspell – Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.</div>
+</div>
+
+<div class="container-fluid px-4 pb-5 pt-5 bgMain"> <!-- Background image layer. See <style> > .bgMain -->
+
+    <div class="row mb-5 justify-content-center">
+        <div class="col-3">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p class="text-uppercase">Moonspell</p>
+                
+                <div class="country-flag ms-2">
+                    <img src="/flags/portugal.svg" alt="Flag of Portugal" />
+                </div>
+            </div>
+
+            <hr />
+            <!--
+            <h2>
+                Portuguese dark metal pioneers discuss their haunting "Memorial" album, creative evolution, and the vibrant Portuguese underground scene.
+            </h2>
+            -->
+            <blockquote class="text-white fs-4">
+                <p>
+                    "We wanted to create an album that was both simple and atmospheric. 'Memorial' is the result of that vision."
+                </p>
+                <!--
+                <p>
+                    "The Portuguese metal scene is thriving. Bands like Manhater and Process of Guilt are pushing boundaries."
+                </p>
+                -->
+            </blockquote>
+            <p class="main-headline-time text-white">Fernando Ribeiro opens up about Moonspell's journey, from crafting their latest masterpiece to winning MTV awards and championing their local metal community. Dive into the depths of one of dark metal's most enduring voices.
+            </p>
+            <p class="main-headline-time text-secondary small">Interviewed by: Nenad Malić.<br />Published on: <datetime>27. 3. 2007.</datetime></p>
+        </div>
+        <div class="col-3 position-relative">
+
+            <div>
+                <img src="/fernando-ribeiro-3110207-photo-by-Jose-Goulao.jpg" alt="Fernando Ribeiro performing in Lisbon, Portugal on 31. 10. 2007."
+                    style="object-fit: contain; object-position: center top; border:1px solid gold;">
+            </div>
+
+
+            <div class="position-absolute bottom-0 end-0 bg-black bg-opacity-75 text-white p-2 small">
+                <div class="mb-0">
+                    <p>Photo by José Goulão <a href="https://creativecommons.org/licenses/by-sa/3.0" class="text-white text-decoration-none">CC BY-SA 3.0</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row gx-5 justify-content-center">
+        <div class="col-md-2 mb-5 px-4">
+            <img src="/one-man-army-and-the-unded-quartet-3.jpg" alt="Johan Lindstrand, vocal of One Man Army and the Unded Quartet" class="img-fluid">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p class="text-uppercase">One Man Army and the Unded Quartet</p>
+                
+                <div class="country-flag ms-2">
+                    <img src="/flags/sweden.svg" alt="Flag of Sweden" />
+                </div>
+            </div>
+            <hr />
+            <h2 class="inter-light">Slipknot's clown is "proud" of Gojira's performance at Olympic Games Opening Ceremony: "I got goosebumps"</h2>
+        </div>
+        
+        <div class="col-md-2 mb-5 px-4">
+            <img src="/chimaira-3.jpg" alt="Chimaira" class="img-fluid">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p class="text-uppercase">CHIMAIRA</p>
+                
+                <div class="country-flag ms-2">
+                    <img src="/flags/united-states.svg" alt="Flag of United States" />
+                </div>
+            </div>
+            <hr />
+            <h2 class="inter-light">John Cooper doesn't care who uses Skillet's music at political rallies: 'I believe in America over Party'</h2>
+        </div>
+ 
+        <div class="col-md-2 mb-5 px-4">
+            <img src="/hammerfall-2.webp" alt="Joacim Cans, Hammerfall vocal" class="img-fluid">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p class="text-uppercase">HAMMERFALL</p>
+                
+                <div class="country-flag ms-2">
+                    <img src="/flags/germany.svg" alt="Flag of Germany" />
+                </div>
+            </div>
+            <hr />
+            <h2 class="inter-light">Manuel Gagneux talks about blending black metal with African-American spirituals</h2>
+        </div>
+
+        <div class="col-md-2 mb-5 px-4">
+            <div class="mt-3 d-flex justify-content-between align-items-start">
+                <p class="text-uppercase" style="margin-top: 0">ENSLAVED</p>
+                
+                <div class="country-flag ms-2">
+                    <img src="/flags/norway.svg" alt="Flag of Norway" />
+                </div>
+            </div>
+            <hr />
+            <h2 style="margin: 0 0 1rem" class="inter-light">Synthwave pioneers discuss their latest album and the intersection of metal and electronic music</h2>
+            <img src="/enslaved-ivar-3.webp" alt="Gunship" class="img-fluid">
+
+        </div>
+    </div>
+</div>
+
+
+
+<style>
+    .container-fluid {
+        background-color:black;
+/*         #4A0202; */
+    }
+    .bgMain {
+        background-image: url('/moonspell-memorial-background-2.jpg');
+        background-repeat: no-repeat;
+        background-position: top left;
+        background-size: 50%;
+    }
+    blockquote p {
+        color: aliceblue;
+        font-size: 2rem;
+        text-align: center;
+        padding: 1rem 0 4.5rem;
+        font-style: italic;
+    }
+    img {
+        border: 1px solid #222;
+    }
+</style>
