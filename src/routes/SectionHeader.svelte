9803db81ab1b13f99190443e425848c30772fb1c
Nenad Malic <4789424+nenadmalic@users.noreply.github.com>
2024-09-16 15:28:53 +0200

    New file for front page headers.

---
+++ src/routes/SectionHeader.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title = '';
+    export let latestContent = '';
+    export let iconSrc = '/skull-dark-purple.png';
+  </script>
+  
+  <div class="d-flex align-items-center bg-white text-black py-4 pb-4">
+      <img src={iconSrc} alt="Section Icon" style="width:2.3rem;height:2.3rem;margin:0 0.5rem 0.4rem 0;">
+      <h1 class="me-3 mb-0"><b>{title}</b></h1>
+      <div class="pb-1 mb-0 text-black">Latest {title.toLowerCase()}: {latestContent}</div>
+  </div>
